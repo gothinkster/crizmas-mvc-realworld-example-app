6e6e77e5bd41903b080e201a1ef0b1efeee85b2a
Raul-Sebastian Mihaila <raul.mihaila@gmail.com>
2017-06-07 08:35:13 +0300

    Init github page

--- src/js/api/tags.js
+++
@@ -1,6 +0,0 @@
-import {get} from 'js/http';
-import {toJson} from 'js/api/utils';
-
-export const getTags = () => {
-  return get('tags').then(toJson);
-};
--- src/js/api/utils.js
+++
@@ -1,7 +0,0 @@
-export const toJson = (response) => response.json();
-
-export const getResponseErrors = (response) => {
-  return response.json().then(({errors}) => {
-    return Promise.reject(Object.entries(errors).map(([key, value]) => `${key} ${value}`));
-  });
-};
